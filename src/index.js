8d583db36b44c0b71c5adf559dc55e97c20f45ad
Ugur Temiz <ugur.temiz@bestseller.com>
2021-04-21 23:01:38 +0200

    secrets didnt work as default

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 try {
   const apiKey = core.getInput('datadog-api-key');
   console.log(`Hello DATADOG_API_KEY ${apiKey}!`);
-  core.setOutput("result", "This is the result!");
+  core.setOutput("result", apiKey);
 } catch (error) {
   core.setFailed(error.message);
 }
