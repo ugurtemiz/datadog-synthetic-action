4ebaf99a8fe5a617b68a4e7846ad4852dc6f062d
Ugur Temiz <ugur.temiz@bestseller.com>
2021-04-21 22:58:16 +0200

    secret test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const core = require('@actions/core');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const apiKey = core.getInput('datadog-api-key');
-  console.log(`Hello ${apiKey}!`);
+  console.log(`Hello DATADOG_API_KEY ${apiKey}!`);
   core.setOutput("result", "This is the result!");
 } catch (error) {
   core.setFailed(error.message);
