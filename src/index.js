84750dd95cc9672ac6715d0767d004ac303840c4
Ugur Temiz <ugur.temiz@bestseller.com>
2021-04-25 17:00:51 +0200

    validation test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const core = require('@actions/core');
+const httpm = require ('@actions/http-client');
 
-try {
-  const apiKey = core.getInput('datadog-api-key');
-  console.log(`Hello DATADOG_API_KEY ${apiKey}!`);
-  core.setOutput("result", apiKey);
-} catch (error) {
-  core.setFailed(error.message);
+function getClient(apiKey) {
+  return new httpm.HttpClient('dd-http-client', [], {
+    headers: {
+      'DD-API-KEY': apiKey,
+      'Content-Type': 'application/json'
+    }
+  })
 }
+
+async function run() {
+  try {
+    const apiKey = core.getInput('datadog-api-key');
+    const apiURL = core.getInput('api-url');
+    console.log(`Hello DATADOG_API_KEY ${apiKey}!`);
+
+    const http = getClient(apiKey);
+
+    const res = await http.get(
+      `${apiURL}/api/v1/validate`,
+      JSON.stringify(s)
+    )
+
+    if (res.message.statusCode === undefined || res.message.statusCode >= 400) {
+      throw new Error(`HTTP request failed: ${res.message.statusMessage}`)
+    }
+
+    core.setOutput("res --> ", JSON.stringify(res));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
